1754935a77ce018cf6d8e21a305771d3f1d205e0
Hardik <19it.hardikprajapati@adit.ac.in>
2020-12-04 10:12:47 +0530

    Near To Finish Challenge 3

--- script.js
+++ script.js
@@ -23,3 +23,66 @@ catGen = () =>{
 }
 
 //Third Challenge
+rpsFun = (YourChoice) =>{
+    HumanChoice = YourChoice.id;
+    BotChoice = Choice(RandomNum());
+    console.log(BotChoice);
+
+    result = Winner(HumanChoice,BotChoice);
+    console.log(result);
+
+    message = Message(result);
+    console.log(message);
+
+    rpsFinal(HumanChoice,BotChoice,message);
+}
+
+RandomNum = () =>{
+    return Math.floor(Math.random() *3);
+}
+
+Choice = (Num) =>{
+    return ["rock","paper","scissors"][Num];
+}
+
+Winner = (HumanChoice,BotChoice) =>{
+    rpsDatabasse = {
+        "rock":{"paper":0,"scissors":1,"rock":0.5},
+        "paper":{"paper":0.5,"scissors":0,"rock":1},
+        "scissors":{"paper":1,"scissors":0.5,"rock":0},
+    };
+
+    let YourChoice = rpsDatabasse[HumanChoice][BotChoice];
+    let ComputerChoice= rpsDatabasse[BotChoice][HumanChoice];
+    return [YourChoice,ComputerChoice];
+}
+
+Message = ([YourChoice,ComputerChoice]) =>{
+    if (YourChoice == 0) {
+        return {"message":"You Lost","color":"red"};
+    }else if(YourChoice == 0.5){
+        return {"message":"You Tied!","color":"yellow"};
+    }else{
+        return {"message":"You Won","color":"green"};
+    }
+}
+
+rpsFinal = (HumanImageChoice,BotImageChoice,message)=>{
+    let image = {
+        "rock":document.getElementById("rock").src,
+        "paper":document.getElementById("paper").src,
+        "scissors":document.getElementById("scissors").src
+    };
+
+    document.getElementById("rock").remove();
+    document.getElementById("paper").remove();
+    document.getElementById("scissors").remove();
+
+    HumanDiv = document.createElement("div");
+    // MessageDiv = document.createElement("div");
+    // BotDiv = document.createElement("div");
+
+    HumanDiv.innerHTML = "<img src='" + image[HumanImageChoice] + "'>";
+
+    document.getElementById("flex-container-3").appendChild(HumanDiv);
+}
