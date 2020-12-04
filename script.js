1f7f8df4df9e4d821c965495ea48a55c7ce34281
Hardik <19it.hardikprajapati@adit.ac.in>
2020-12-04 17:14:44 +0530

    Forth Project Done

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // First Challenge
-ageInDays = ()=>{
+ageInDays = () => {
     let age = prompt("What Is Your Age :-");
-    let AgeInDays = (age)* 365;
+    let AgeInDays = (age) * 365;
     let h1 = document.createElement("h1");
     let TextAns = document.createTextNode("Your Age is " + AgeInDays + " in Days");
-    h1.setAttribute("id","ageInDays");
+    h1.setAttribute("id", "ageInDays");
     h1.appendChild(TextAns);
     console.log(h1);
     document.getElementById("flex-result-1").appendChild(h1);
 }
 
-reset = () =>{
+reset = () => {
     document.getElementById("ageInDays").remove();
 }
 
 //Second Challenge
-catGen = () =>{
+catGen = () => {
     let image = document.createElement("img");
     let div = document.getElementById("cat");
     image.src = "https://source.unsplash.com/200x125/?cat";
@@ -23,55 +23,55 @@ catGen = () =>{
 }
 
 //Third Challenge
-rpsFun = (YourChoice) =>{
+rpsFun = (YourChoice) => {
     HumanChoice = YourChoice.id;
     BotChoice = Choice(RandomNum());
-    console.log(BotChoice);
+    // console.log(BotChoice);
 
-    result = Winner(HumanChoice,BotChoice);
-    console.log(result);
+    result = Winner(HumanChoice, BotChoice);
+    // console.log(result);
 
     message = Message(result);
-    console.log(message);
+    // console.log(message);
 
-    rpsFinal(HumanChoice,BotChoice,message);
+    rpsFinal(HumanChoice, BotChoice, message);
 }
 
-RandomNum = () =>{
-    return Math.floor(Math.random() *3);
+RandomNum = () => {
+    return Math.floor(Math.random() * 3);
 }
 
-Choice = (Num) =>{
-    return ["rock","paper","scissors"][Num];
+Choice = (Num) => {
+    return ["rock", "paper", "scissors"][Num];
 }
 
-Winner = (HumanChoice,BotChoice) =>{
+Winner = (HumanChoice, BotChoice) => {
     rpsDatabasse = {
-        "rock":{"paper":0,"scissors":1,"rock":0.5},
-        "paper":{"paper":0.5,"scissors":0,"rock":1},
-        "scissors":{"paper":1,"scissors":0.5,"rock":0},
+        "rock": { "paper": 0, "scissors": 1, "rock": 0.5 },
+        "paper": { "paper": 0.5, "scissors": 0, "rock": 1 },
+        "scissors": { "paper": 1, "scissors": 0.5, "rock": 0 },
     };
 
     let YourChoice = rpsDatabasse[HumanChoice][BotChoice];
-    let ComputerChoice= rpsDatabasse[BotChoice][HumanChoice];
-    return [YourChoice,ComputerChoice];
+    let ComputerChoice = rpsDatabasse[BotChoice][HumanChoice];
+    return [YourChoice, ComputerChoice];
 }
 
-Message = ([YourChoice,ComputerChoice]) =>{
+Message = ([YourChoice, ComputerChoice]) => {
     if (YourChoice == 0) {
-        return {"message":"You Lost!","color":"red"};
-    }else if(YourChoice == 0.5){
-        return {"message":"You Tied!","color":"yellow"};
-    }else{
-        return {"message":"You Won!","color":"green"};
+        return { "message": "You Lost!", "color": "red" };
+    } else if (YourChoice == 0.5) {
+        return { "message": "You Tied!", "color": "yellow" };
+    } else {
+        return { "message": "You Won!", "color": "green" };
     }
 }
 
-rpsFinal = (HumanImageChoice,BotImageChoice,message)=>{
+rpsFinal = (HumanImageChoice, BotImageChoice, message) => {
     let image = {
-        "rock":document.getElementById("rock").src,
-        "paper":document.getElementById("paper").src,
-        "scissors":document.getElementById("scissors").src
+        "rock": document.getElementById("rock").src,
+        "paper": document.getElementById("paper").src,
+        "scissors": document.getElementById("scissors").src
     };
 
     document.getElementById("rock").remove();
@@ -89,4 +89,54 @@ rpsFinal = (HumanImageChoice,BotImageChoice,message)=>{
     document.getElementById("flex-container-3").appendChild(HumanDiv);
     document.getElementById("flex-container-3").appendChild(MessageDiv);
     document.getElementById("flex-container-3").appendChild(BotDiv);
+}
+
+//Forth Challenge 
+let AllButton = document.getElementsByTagName("button");
+
+let ColorHistory = [];
+for (let i = 0; i < AllButton.length; i++) {
+    ColorHistory.push(AllButton[i].classList[1]);
+}
+
+ColorChange = (ButtonColor) =>{
+    if (ButtonColor.value == "red") {
+        ButtonRed();
+    }else if(ButtonColor.value == "green"){
+        ButtonGreen();
+    }else if(ButtonColor.value == "random"){
+        ButtonRandom();
+    }else{
+        ButtonReset();
+    }
+}
+
+ButtonRed = () =>{
+    for (let i = 0; i < AllButton.length; i++) {
+        AllButton[i].classList.remove(AllButton[i].classList[1]);
+        AllButton[i].classList.add("btn-danger");
+    }
+}
+
+ButtonGreen = () =>{
+    for (let i = 0; i < AllButton.length; i++) {
+        AllButton[i].classList.remove(AllButton[i].classList[1]);
+        AllButton[i].classList.add("btn-success");
+    }
+}
+
+ButtonReset = () =>{
+    for (let i = 0; i < AllButton.length; i++) {
+        AllButton[i].classList.remove(AllButton[i].classList[1]);
+        AllButton[i].classList.add(ColorHistory[i]);
+    }
+}
+
+ButtonRandom = () =>{
+    let Choice = ["btn-primary","btn-success","btn-warning","btn-danger"];
+    for (let i = 0; i < AllButton.length; i++) {
+        let Random = Math.floor(Math.random() *4)
+        AllButton[i].classList.remove(AllButton[i].classList[1]);
+        AllButton[i].classList.add(Choice[Random]);
+    }
 }
